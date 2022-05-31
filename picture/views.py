85d7dac34d6916efb919b50154633ef145c8fc43
Clemo97 <lumumbaclement@gmail.com>
2022-05-31 13:31:43 +0300

    welcome  view function

--- picture/views.py
+++ picture/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
+from .models import Picture,Location,Category
 
 # Create your views here.
+def welcome(request):
+    pictures = Picture.objects.all()
+    kenya=Picture.filter_by_location(location='Kenya')
+    japan=Picture.filter_by_location(location='Japan')
+    mozambique=Picture.filter_by_location(location='Mozambique')
+    iceland=Picture.filter_by_location(location='Iceland')
+    return render(request,'all-pictures/index.html',{'pictures':pictures, 'kenya':kenya,'japan':japan,'mozambique':mozambique,'iceland':iceland})
